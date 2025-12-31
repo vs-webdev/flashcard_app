5cc4a945a130463bf7f9cb51bd70c6560f62133b
Vikas Solanki <solankivikas066@gmail.com>
2025-12-31 22:48:17 +0530

    feat(header): add logo image and update style

--- src/features/navigation/Header.tsx
+++ src/features/navigation/Header.tsx
@@ -1,10 +1,14 @@
 import ViewToggle from './ViewToggle'
+import logo from '../../assets/ChatGPT Image Dec 31, 2025, 08_08_35 PM.png'
 
 const Header = () => {
   return (
-    <header className='flex items-center justify-between'>
-      <div>
-        <span>Flashcard</span>
+    <header className='flex items-center justify-between mb-5'>
+      <div className='flex items-center gap-2'>
+        <div className='w-10 h-10 overflow-hidden'>
+          <img src={logo} alt="" className='w-full scale-160 object-cover mt-[2px]'/>
+        </div>
+        <span className='text-2xl font-bold'>Flashcard</span>
       </div>
 
       <ViewToggle />
--- src/features/navigation/ViewToggle.tsx
+++ src/features/navigation/ViewToggle.tsx
@@ -6,7 +6,7 @@ const ViewToggle = () => {
   const [toggleViewState, setToggleViewState] = useState<ToggleMode>('study')
 
   return (
-    <div className="border-2 relative p-1 text-sm rounded-full">
+    <div className="border-2 relative p-1 text-sm rounded-full bg-[var(--color-white)] shadow-[1px_2px_0_0_var(--color-maroon)]">
       <div className="absolute top-1 bottom-1 bg-[var(--color-amber)] border rounded-full transition-all duration-500 ease-out" 
         style={{
           left: toggleViewState === 'study' ? '0.3rem' : '50%',
@@ -15,13 +15,13 @@ const ViewToggle = () => {
       />
       <button 
         onClick={() => setToggleViewState('study')}
-        className="w-26 py-1 transition-all duration-500 relative z-10 cursor-pointer"
+        className="w-32 py-1 px-1 text-lg font-[500] transition-all duration-500 relative z-10 cursor-pointer"
       >
         Study Mode
       </button>
       <button
         onClick={() => setToggleViewState('allCards')}
-        className="w-26 py-1 transition-all duration-500 relative z-10 cursor-pointer"
+        className="w-32 py-1 px-1 text-lg font-[500] transition-all duration-500 relative z-10 cursor-pointer"
       >
         All Cards
       </button>
