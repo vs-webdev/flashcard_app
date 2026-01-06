f7bcf52407361cca5c785e95dce7b9c830db853b
Vikas Solanki <solankivikas066@gmail.com>
2026-01-06 23:37:07 +0530

    chore(types): add shared common Card types

---
+++ src/shared/types/common.types.ts
@@ -0,0 +1,9 @@
+export interface Card {
+  id: string;
+  question: string;
+  answer: string;
+  category: string;
+  masteryLevel: CardMastery;
+}
+
+export type CardMastery = 0 | 1 | 2 | 3 | 4 | 5
