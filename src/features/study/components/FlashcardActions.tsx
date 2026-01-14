c40c8c71cc646b94708f776aa27c898ad9cc897e
Vikas Solanki <solankivikas066@gmail.com>
2026-01-14 19:52:57 +0530

    fix(FlashcardActions): increment mastery level progressively on know action

--- src/features/study/components/FlashcardActions.tsx
+++ src/features/study/components/FlashcardActions.tsx
@@ -1,25 +1,32 @@
+import type { FC } from "react";
+import type { Card, CardMastery } from "../../../shared/types/common.types";
+import { useShallow } from "zustand/shallow";
+import { useFlashcardStore } from "../../../store/store";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { RiResetLeftLine } from "react-icons/ri";
-import { useFlashcardStore } from "../../../store/store";
-import { useShallow } from "zustand/shallow";
-import type { FC } from "react";
 
 interface FlashcardActionsProps {
-  cardId: string,
-  masteryLevel: number,
+  card: Card,
 }
 
-const FlashcardActions: FC<FlashcardActionsProps> = ({cardId, masteryLevel}) => {
+const FlashcardActions: FC<FlashcardActionsProps> = ({card}) => {
   const {updateCard} = useFlashcardStore(useShallow(state => ({
     updateCard: state.updateCard
   })))
+
+  const updateMastery = () => {
+    const newLevel = Math.min(5, card?.masteryLevel + 1) as CardMastery;
+    updateCard(card?.id, {masteryLevel: newLevel})
+  }
+
+  const resetMastery = () => updateCard(card?.id, {masteryLevel: 0});
 
   return (
     <div className='flex justify-center items-center text-lg gap-6 mt-6'>
       <button 
         className='flex items-center gap-2 border rounded-full disabled:opacity-75 font-[500] py-2 px-6 bg-[var(--color-amber)] shadow-[2px_2px_0_1px_var(--color-maroon)]'
-        onClick={() => updateCard(cardId, {masteryLevel: 5})}
-        disabled={masteryLevel === 5}
+        onClick={updateMastery}
+        disabled={card?.masteryLevel === 5}
       >
         <FaRegCheckCircle />
         I Know This
@@ -27,8 +34,8 @@ const FlashcardActions: FC<FlashcardActionsProps> = ({cardId, masteryLevel}) => 
 
       <button 
         className='flex items-center gap-2 border rounded-full disabled:opacity-75 font-[500] py-2 px-6 bg-[var(--color-white)] shadow-[2px_2px_0_1px_var(--color-maroon)]'
-        onClick={() => updateCard(cardId, {masteryLevel: 0})}
-        disabled={masteryLevel === 0}
+        onClick={resetMastery}
+        disabled={card?.masteryLevel === 0}
       >
         <RiResetLeftLine />
         Reset Progress
