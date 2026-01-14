ce6832c824ad16d3c717e8c8b59f6ea368af0ef5
Vikas Solanki <solankivikas066@gmail.com>
2026-01-14 18:56:53 +0530

    fix(FlashcardActions): disable mastery actions based on current level

--- src/features/study/components/FlashcardActions.tsx
+++ src/features/study/components/FlashcardActions.tsx
@@ -6,9 +6,10 @@ import type { FC } from "react";
 
 interface FlashcardActionsProps {
   cardId: string,
+  masteryLevel: number,
 }
 
-const FlashcardActions: FC<FlashcardActionsProps> = ({cardId}) => {
+const FlashcardActions: FC<FlashcardActionsProps> = ({cardId, masteryLevel}) => {
   const {updateCard} = useFlashcardStore(useShallow(state => ({
     updateCard: state.updateCard
   })))
@@ -16,16 +17,18 @@ const FlashcardActions: FC<FlashcardActionsProps> = ({cardId}) => {
   return (
     <div className='flex justify-center items-center text-lg gap-6 mt-6'>
       <button 
-        className='flex items-center gap-2 border rounded-full font-[500] py-2 px-6 bg-[var(--color-amber)] shadow-[2px_2px_0_1px_var(--color-maroon)]'
+        className='flex items-center gap-2 border rounded-full disabled:opacity-75 font-[500] py-2 px-6 bg-[var(--color-amber)] shadow-[2px_2px_0_1px_var(--color-maroon)]'
         onClick={() => updateCard(cardId, {masteryLevel: 5})}
+        disabled={masteryLevel === 5}
       >
         <FaRegCheckCircle />
         I Know This
       </button>
 
       <button 
-        className='flex items-center gap-2 border rounded-full font-[500] py-2 px-6 bg-[var(--color-white)] shadow-[2px_2px_0_1px_var(--color-maroon)]'
+        className='flex items-center gap-2 border rounded-full disabled:opacity-75 font-[500] py-2 px-6 bg-[var(--color-white)] shadow-[2px_2px_0_1px_var(--color-maroon)]'
         onClick={() => updateCard(cardId, {masteryLevel: 0})}
+        disabled={masteryLevel === 0}
       >
         <RiResetLeftLine />
         Reset Progress
